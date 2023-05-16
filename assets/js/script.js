3c55918d067a7ba84141439c316396d1992df556
Flavio Pinto <flaviopnt@hotmail.it>
2023-05-16 19:36:12 +0200

    [FEAT]Elements created with DOM manipulation

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,41 @@
+/* Creazione elementi nel DOM */
+
+const body = document.querySelector("body");
+const mainElement = document.createElement("main");
+const footerElement = document.createElement("footer");
+
+mainElement.classList.add("counter");
+footerElement.classList.add('footer');
+footerElement.textContent = "Made with ❤️ by Flavio";
+body.prepend(mainElement);
+mainElement.insertAdjacentElement('afterend', footerElement);
+
+const divCounterNumber = document.createElement("div");
+const divCounterButtons = document.createElement("div");
+
+divCounterNumber.classList.add("counter__number");
+divCounterButtons.classList.add("counter__buttons");
+
+mainElement.append(divCounterNumber, divCounterButtons);
+
+divCounterNumber.textContent = "0";
+
+const buttonPlus = document.createElement("button");
+const buttonReset = document.createElement("button");
+const buttonMinus = document.createElement("button");
+
+buttonPlus.classList.add("counter__button");
+buttonReset.classList.add("counter__button");
+buttonMinus.classList.add("counter__button");
+
+buttonPlus.textContent = "+";
+buttonReset.textContent = "Reset";
+buttonMinus.textContent = "-";
+
+divCounterButtons.append(buttonPlus, buttonReset, buttonMinus);
+
+/* Interattività pulsanti */
+
 let currentCount = document.querySelector(".counter__number");
 let buttons = document.querySelectorAll(".counter__button");
 
@@ -23,15 +61,14 @@ function operationHandler(btn) {
 }
 
 function resetHandler() {
-
-  for(let i = 0; i < Math.abs(currentCount.innerText); i++) {
+  for (let i = 0; i < Math.abs(currentCount.innerText); i++) {
     setTimeout(() => {
       if (currentCount.innerText > 0) {
-        currentCount.innerText--
+        currentCount.innerText--;
       }
 
-      if(currentCount.innerText < 0) {
-        currentCount.innerText++
+      if (currentCount.innerText < 0) {
+        currentCount.innerText++;
       }
     }, 30 * i);
   }
